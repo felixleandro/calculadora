8eb440b4028ec6fda084f9eacf609061971a45f3
Brunomatrix <brunomatrix1986@hotmail.com>
2016-05-10 21:34:11 -0300

    novo arquivo

--- src/calc/calculadora.java
+++ src/calc/calculadora.java
@@ -1,5 +1,24 @@
 package calc;
 
 public class calculadora {
+	
+	
+	
+	public double somar (int parametro1,int parametro2){
+	return parametro1 + parametro2;	
+	
+	}
+	
+	public double subtrair (int parametro1 ,int parametro2){
+		return parametro1 - parametro2;
+	}
+	
+	public double multiplicar (int parametro1, int parametro2){
+		return parametro1 * parametro2;
+	}
+	
+	public double dividir (int parametro1, int parametro2){
+		return parametro1 / parametro2;
+	}
 
 }
--- src/calc/calculadora2.java
+++ src/calc/calculadora2.java
@@ -1,10 +1,19 @@
 package calc;
 
+// tag 1.0.0
+
 public class calculadora2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		calculadora c = new calculadora();
+		
+		System.out.println(c.somar(4,2));
+		System.out.println(c.subtrair(5,4));
+		System.out.println(c.multiplicar(10,5));
+		System.out.println(c.dividir(2,8));
 	}
 
 }
