a6f862ff9c718148fc5c9a029f8a44773284ed68
Brunomatrix <brunomatrix1986@hotmail.com>
2016-05-10 21:38:05 -0300

    novo

--- src/calc/calculadora.java
+++ src/calc/calculadora.java
@@ -5,20 +5,20 @@ public class calculadora {
 	
 	
 	public double somar (int parametro1,int parametro2){
-	return parametro1 + parametro2;	
+	return parametro1 * parametro2;	
 	
 	}
 	
 	public double subtrair (int parametro1 ,int parametro2){
-		return parametro1 - parametro2;
+		return parametro1 + parametro2;
 	}
 	
 	public double multiplicar (int parametro1, int parametro2){
-		return parametro1 * parametro2;
+		return parametro1 / parametro2;
 	}
 	
 	public double dividir (int parametro1, int parametro2){
-		return parametro1 / parametro2;
+		return parametro1 - parametro2;
 	}
 
 }
